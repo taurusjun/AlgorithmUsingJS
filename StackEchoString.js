b14aff1c6b735120fd4a683830754ecdabd65454
jeremy.wang <jeremy.wang01@sap.com>
2016-07-11 10:29:26 +0800

    use stack

---
+++ StackEchoString.js
@@ -0,0 +1,26 @@
+/**
+ * Created by I306067 on 7/11/2016.
+ */
+var Stack=require('./Stack.js').Stack;
+
+var isEchoString=function (str) {
+    
+    console.log("in: "+str);
+    var stack = new Stack();
+    for(var indx=0; indx < str.length; indx++){
+        stack.push(str[indx]);
+    }
+    
+    var reversStr=""
+    while (stack.length()>0){
+        reversStr+=stack.pop();
+    }
+
+    console.log("revers: "+reversStr);
+    return str===reversStr;
+}
+
+console.log(isEchoString('eer'));
+console.log(isEchoString('er'));
+console.log(isEchoString('ere'));
+console.log(isEchoString('erer'));
